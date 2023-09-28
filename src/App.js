dc308bbce69a77d5c3584017d7e6140f8d76965c
Anjalijp-2000 <anjalijp297@gmail.com>
2023-09-28 11:50:34 +0530

    video

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Home from './pages/Home';
+import LandingPage from './pages/LandingPage';
+function App() {
+  return (
+    <div className="App">
+      <h1>Veedio App</h1>
+      <LandingPage/>
+      <Home/>
+      
+      
+    </div>
+  );
+}
+
+export default App
